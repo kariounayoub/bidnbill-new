bd2eaba6a80c54a386f6d743c86d3eaf81d84cfc
Majid Jaidi <majid.jaidi@gmail.com>
2019-05-10 13:09:49 +0000

    front end sign up sign in

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+import "particles.js";
+
+particlesJS.load('login-form', '../assets/stylesheets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
